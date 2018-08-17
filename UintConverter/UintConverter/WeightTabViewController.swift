acd006df5dd2461d637194e0ba2e732fda9def8e
Tharindu Perera <tharindu.madushanka@my.sliit.lk>
2018-08-18 01:30:32 +0530

    set Editing Changed events for each TextField in Weight ViewController || added TextField clear mechanism

--- UintConverter/UintConverter/WeightTabViewController.swift
+++ UintConverter/UintConverter/WeightTabViewController.swift
@@ -8,12 +8,55 @@
 
 import UIKit
 
-class WeightTabViewController: UIViewController {
+class WeightTabViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var gramTextField: UITextField!
+    @IBOutlet weak var kgTextField: UITextField!
+    @IBOutlet weak var poundTextField: UITextField!
+    @IBOutlet weak var ounceTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.hideKeyboardWhenTappedAround()
+        self.initDelegates()
     }
+    
+    private func initDelegates(){
+        gramTextField.delegate = self
+        kgTextField.delegate = self
+        poundTextField.delegate = self
+        ounceTextField.delegate = self
+    }
+    
+    private func clearTextFields(){
+        gramTextField.text = ""
+        kgTextField.text = ""
+        poundTextField.text = ""
+        ounceTextField.text = ""
+    }
+    
+    //MARK: UITextField Editing Changed Events
+    
+    @IBAction func onGramValueChanged(_ sender: UITextField) {
+    }
+    
+    @IBAction func onKgValueChanged(_ sender: UITextField) {
+    }
+    
+    @IBAction func onPoundValueChanged(_ sender: UITextField) {
+    }
+    
+    
+    @IBAction func onOunceValueChanged(_ sender: Any) {
+    }
+    
+    
+    //MARK: UITextFieldDelegate
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        clearTextFields()
+    }
+    
     
 }
 
