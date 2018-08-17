9de339ffe3cab1362db2e458724a88a642830cf6
Tharindu Perera <tharindu.madushanka@my.sliit.lk>
2018-08-17 22:34:13 +0530

    added an extension to hide keyboard when outside is touched for UIViewController

---
+++ UintConverter/UintConverter/Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  Extensions.swift
+//  UintConverter
+//
+//  Created by Tharindu Perera on 8/17/18.
+//  Copyright © 2018 Tharindu Perera. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
